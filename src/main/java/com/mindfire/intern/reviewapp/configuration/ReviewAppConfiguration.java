e4f5b791c809c0d66c0ff1668fa0e04d6d5506d9
Ashutosh-Dash <ashutosh.dash@mindfiresolutions.com>
2016-02-17 09:47:21 +0530

    Added some resources

--- src/main/java/com/mindfire/intern/reviewapp/configuration/ReviewAppConfiguration.java
+++ src/main/java/com/mindfire/intern/reviewapp/configuration/ReviewAppConfiguration.java
@@ -27,7 +27,7 @@ public class ReviewAppConfiguration extends WebMvcConfigurerAdapter {
 	
 	@Override
 	public void addResourceHandlers(final ResourceHandlerRegistry registry) {
-		registry.addResourceHandler("/resources/**").addResourceLocations("/WEB-INF/resources/");
+		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
 	}
 
 }
